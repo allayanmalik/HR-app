1c771b2e602534050ba9181a8e18424ab18a24cd
Allayan Malik <allayanmalik@gmail.com>
2026-08-08 02:32:23 +0100

    Require SSL for PostgreSQL connections

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ export const dbConfig = {
   user: process.env.DB_USER || DEFAULT_DB_USER,
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || DEFAULT_DB_NAME,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : undefined
+  ssl: { rejectUnauthorized: false }
 };
 
 export const pool = new Pool(dbConfig);
